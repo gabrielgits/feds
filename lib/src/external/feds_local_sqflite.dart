2d140b017db2c887060d16f9664edeb3be02f4f6
Gabriel Vieira <72738617+gabrielgits@users.noreply.github.com>
2024-04-07 19:25:25 +0100

    create file for http and sqflite implementation

---
+++ lib/src/external/feds_local_sqflite.dart
@@ -0,0 +1,80 @@
+import '../feds_local.dart';
+
+class FedsLocalSqflite implements FedsLocal {
+  @override
+  Future<int> delete({required int id, required String table}) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> deleteAll(String table) {
+    // TODO: implement deleteAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Map<String, dynamic>>> getAll(String table) {
+    // TODO: implement getAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, dynamic>> getItem(
+      {required int id, required String table}) {
+    // TODO: implement getItem
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> save(
+      {required Map<String, dynamic> item, required String table}) {
+    // TODO: implement save
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> saveAll(
+      {required List<Map<String, dynamic>> items, required String table}) {
+    // TODO: implement saveAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, dynamic>> search(
+      {required String table, required String criteria}) {
+    // TODO: implement search
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Map<String, dynamic>>> searchAll(
+      {required String table,
+      required String criteria,
+      List<Object?>? criteriaListData}) {
+    // TODO: implement searchAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> searchDelete(String table, String criteria) {
+    // TODO: implement searchDelete
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> searchUpdate(
+      {required String table,
+      required String criteria,
+      required Map<String, dynamic> item}) {
+    // TODO: implement searchUpdate
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> update(
+      {required Map<String, dynamic> item, required String table}) {
+    // TODO: implement update
+    throw UnimplementedError();
+  }
+}
---
+++ lib/src/external/feds_rest_http.dart
@@ -0,0 +1,27 @@
+import 'package:feds/feds.dart';
+
+class FedsRestHttp implements FedsRest {
+  @override
+  Future<Map<String, dynamic>> get(String url) {
+    // TODO: implement get
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<int>> getData(String url) {
+    // TODO: implement getData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, dynamic>> post(String url, Map<String, dynamic> body) {
+    // TODO: implement post
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Map<String, dynamic>> put(String url, Map<String, dynamic> body) {
+    // TODO: implement put
+    throw UnimplementedError();
+  }
+}
