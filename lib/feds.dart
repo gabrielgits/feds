9e87c212b6f85776ed98418c03efa52c73796a7d
Gabriel Vieira <72738617+gabrielgits@users.noreply.github.com>
2024-04-07 19:35:39 +0100

    export http and sqflite implementation

--- lib/feds.dart
+++ lib/feds.dart
@@ -2,6 +2,8 @@ library feds;
 
 export 'package:feds/src/feds_local.dart';
 export 'package:feds/src/external/feds_local_shared_pref.dart';
+export 'package:feds/src/external/feds_local_sqflite.dart';
 
 export 'package:feds/src/feds_rest.dart';
 export 'package:feds/src/external/feds_rest_dio.dart';
+export 'package:feds/src/external/feds_rest_http.dart';
