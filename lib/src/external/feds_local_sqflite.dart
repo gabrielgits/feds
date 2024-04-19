199d2dcbdd27dfc93dbf5617deffd0c6e8072374
Gabriel Vieira <72738617+gabrielgits@users.noreply.github.com>
2024-04-19 12:11:46 +0100

    update FedsLocalSqflite to easy test and create first test

--- lib/src/external/feds_local_sqflite.dart
+++ lib/src/external/feds_local_sqflite.dart
@@ -6,14 +6,14 @@ import 'package:sqflite/sqflite.dart';
 import '../feds_local.dart';
 
 class FedsLocalSqflite implements FedsLocal {
-  static Database? _database;
+  static Database? database;
   final String dbPath;
 
   const FedsLocalSqflite({required this.dbPath});
 
   Future<Database?> initDatabase(String dbPathLocation) async {
-    if (_database != null) {
-      return _database;
+    if (database != null) {
+      return database;
     }
 
     bool fileCreated = await File(dbPathLocation).exists();
@@ -23,15 +23,14 @@ class FedsLocalSqflite implements FedsLocal {
           data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
       await File(dbPathLocation).writeAsBytes(bytes);
     }
-    _database = await openDatabase(dbPathLocation);
-    return _database;
+    database = await openDatabase(dbPathLocation);
+    return database;
   }
 
   @override
   Future<int> delete({required int id, required String table}) async {
-    final db = await initDatabase(dbPath);
-    if (db != null &&
-        await db.delete(table, where: 'id = ?', whereArgs: [id]) > 0) {
+    if (database != null &&
+        await database!.delete(table, where: 'id = ?', whereArgs: [id]) > 0) {
       return id;
     }
     return 0;
@@ -39,9 +38,8 @@ class FedsLocalSqflite implements FedsLocal {
 
   @override
   Future<int> deleteAll(String table) async {
-    final db = await initDatabase(dbPath);
-    if (db != null) {
-      await db.execute('DELETE FROM $table');
+    if (database != null) {
+      await database!.execute('DELETE FROM $table');
       return 1;
     }
     return 0;
@@ -49,9 +47,8 @@ class FedsLocalSqflite implements FedsLocal {
 
   @override
   Future<List<Map<String, dynamic>>> getAll(String table) async {
-    final db = await initDatabase(dbPath);
-    if (db != null) {
-      final list = await db.query(table);
+    if (database != null) {
+      final list = await database!.query(table);
       if (list.isNotEmpty) {
         return list;
       }
@@ -62,9 +59,9 @@ class FedsLocalSqflite implements FedsLocal {
   @override
   Future<Map<String, dynamic>> getItem(
       {required int id, required String table}) async {
-    final db = await initDatabase(dbPath);
-    if (db != null) {
-      final list = await db.query(table, where: 'id = ?', whereArgs: [id]);
+    if (database != null) {
+      final list =
+          await database!.query(table, where: 'id = ?', whereArgs: [id]);
       if (list.isNotEmpty) {
         return list[0];
       }
@@ -75,8 +72,7 @@ class FedsLocalSqflite implements FedsLocal {
   @override
   Future<int> save(
       {required Map<String, dynamic> item, required String table}) async {
-    final db = await initDatabase(dbPath);
-    if (db != null && await db.insert(table, item) > 0) {
+    if (database != null && await database!.insert(table, item) > 0) {
       return item['id'];
     }
     return 0;
@@ -86,11 +82,10 @@ class FedsLocalSqflite implements FedsLocal {
   Future<int> saveAll(
       {required List<Map<String, dynamic>> items,
       required String table}) async {
-    final db = await initDatabase(dbPath);
     int quant = 0;
-    if (db != null) {
+    if (database != null) {
       for (var element in items) {
-        final result = await db.insert(table, element);
+        final result = await database!.insert(table, element);
         if (result < 1) {
           return quant;
         }
@@ -103,9 +98,8 @@ class FedsLocalSqflite implements FedsLocal {
   @override
   Future<Map<String, dynamic>> search(
       {required String table, required String criteria}) async {
-    final db = await initDatabase(dbPath);
-    if (db != null) {
-      final list = await db.query(table, where: criteria);
+    if (database != null) {
+      final list = await database!.query(table, where: criteria);
       if (list.isNotEmpty) {
         return list[0];
       }
@@ -118,9 +112,8 @@ class FedsLocalSqflite implements FedsLocal {
       {required String table,
       required String criteria,
       List<Object?>? criteriaListData}) async {
-    final db = await initDatabase(dbPath);
-    if (db != null) {
-      final list = await db.query(
+    if (database != null) {
+      final list = await database!.query(
         table,
         where: criteria,
         whereArgs: criteriaListData,
@@ -134,9 +127,8 @@ class FedsLocalSqflite implements FedsLocal {
 
   @override
   Future<int> searchDelete(String table, String criteria) async {
-    final db = await initDatabase(dbPath);
-    if (db != null) {
-      return await db.delete(table, where: criteria);
+    if (database != null) {
+      return await database!.delete(table, where: criteria);
     }
     return 0;
   }
@@ -146,10 +138,8 @@ class FedsLocalSqflite implements FedsLocal {
       {required String table,
       required String criteria,
       required Map<String, dynamic> updateItem}) async {
-    final db = await initDatabase(dbPath);
-
-    if (db != null) {
-      return await db.update(table, updateItem, where: criteria);
+    if (database != null) {
+      return await database!.update(table, updateItem, where: criteria);
     }
     return 0;
   }
@@ -157,11 +147,13 @@ class FedsLocalSqflite implements FedsLocal {
   @override
   Future<int> update(
       {required Map<String, dynamic> item, required String table}) async {
-    final db = await initDatabase(dbPath);
-
-    if (db != null) {
-      return await db
-          .update(table, item, where: 'id = ?', whereArgs: [item['id']]);
+    if (database != null) {
+      return await database!.update(
+        table,
+        item,
+        where: 'id = ?',
+        whereArgs: [item['id']],
+      );
     }
     return 0;
   }
